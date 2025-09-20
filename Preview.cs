3b04d1abda7529b769908acc3383eb5b924d4188
AndreEvdokimov <touchtomuch13@gmail.com>
2025-09-20 14:19:04 +0300

    Добавил класс Preview для вывода таблиц в консоль

---
+++ Preview.cs
@@ -0,0 +1,42 @@
+﻿/// <summary>
+/// Предпросмотр данных в консоли
+/// </summary>
+internal static class Preview
+{
+    /// <summary>
+    /// Выводит таблицу в консоль
+    /// </summary>
+    /// <param name="data"></param>
+    /// <param name="printedRows"></param>
+    internal static void PrintTable(in DataSet data, in int? printedRows = null)
+    {
+        for (int i = 0; i < data.Count; i++)
+        {
+            Console.Write(data[i].Name + "    ");
+        }
+
+        for (int i = 0; i < data[0].Count; i++)
+        {
+            Console.WriteLine();
+
+            for (int j = 0; j < data.Count; j++)
+            {
+                Console.Write(data[j][i] + "    "); // поля
+                //Console.Write(data[i][j]); // строки
+
+                //if (i < data.Count)
+                //{
+                //    Console.Write(',');
+                //}
+            }
+
+            if (printedRows != null && i == printedRows)
+            {
+                return;
+            }
+        }
+        
+        Console.WriteLine();
+    }
+
+}
