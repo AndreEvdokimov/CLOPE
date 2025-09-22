35d0c4f75eab15c5d425c6107e0f8461908aec81
AndreEvdokimov <touchtomuch13@gmail.com>
2025-09-23 01:13:53 +0300

    Метод PrintTable перенес в хелперы. Класс Preview удалил

--- Helpers.cs
+++ Helpers.cs
@@ -139,4 +139,34 @@ internal class Helpers
         }
     }
 
+    /// <summary>
+    /// Выводит таблицу в консоль
+    /// </summary>
+    /// <param name="data"></param>
+    /// <param name="printedRows"></param>
+    internal static void PrintTable(in DataSet data, in int? printedRows = null)
+    {
+        for (int i = 0; i < data.Count; i++)
+        {
+            Console.Write(data[i].Name + "    ");
+        }
+
+        for (int i = 0; i < data[0].Count; i++)
+        {
+            Console.WriteLine();
+
+            for (int j = 0; j < data.Count; j++)
+            {
+                Console.Write(data[j][i] + "    "); // поля
+            }
+
+            if (printedRows != null && i == printedRows)
+            {
+                return;
+            }
+        }
+
+        Console.WriteLine();
+    }
+
 }
